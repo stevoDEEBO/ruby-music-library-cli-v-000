d96b2bc0d0e7a2b4bdbf9273c48461b51a0a83f6
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-28 08:34:03 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -74,7 +74,6 @@ class MusicLibraryController
     if (1..Song.all.length).include?(song_number)
       song = song_list[song_number-1]
       puts "Playing #{song.name} by #{song.artist.name}"
-      binding.pry
     end
   end
 end
